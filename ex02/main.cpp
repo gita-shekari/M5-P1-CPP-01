c171c8dc18409657f3624d6553909aec4284b105
Gita Shekari <gshekari@f0r3s26.codam.nl>
2026-04-14 16:36:37 +0200

    update

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,29 @@
 /*   By: gshekari <gshekari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 16:18:19 by gshekari          #+#    #+#             */
-/*   Updated: 2026/04/14 16:18:20 by gshekari         ###   ########.fr       */
+/*   Updated: 2026/04/14 16:35:51 by gshekari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	 std::string str = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Memory address of str:" << &str << std::endl;
+	std::cout << "Memory address of PTR:" << stringPTR << std::endl;
+	std::cout << "Memory address of REF:" << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of PTR: " << *stringPTR << std::endl;
+	std::cout << "Value of REF: " << stringREF << std::endl;
+
+	return 0;
+}
